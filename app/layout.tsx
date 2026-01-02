8440d334a131c44e3384f2d9b74f8a0daa7af3a0
rayfromroblox <museeraymond@gmail.com>
2026-01-02 22:40:17 +0300

    Add explicit favicon link tags in HTML head

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
+      </head>
       <body className={`${inter.variable} font-inter antialiased bg-black text-white min-h-screen overflow-x-hidden`} suppressHydrationWarning>
         <SWRProvider>
           <Preloader />
